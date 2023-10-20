2a5be555bb01b7d5b466aa0a3f4976db87aebd35
Garcon2Jeu <104232891+Garcon2Jeu@users.noreply.github.com>
2023-10-20 15:41:21 +0200

    Main & App setup

---
+++ App.lua
@@ -0,0 +1,13 @@
+Class = require "libraries.class"
+Push = require "libraries.push"
+
+App = Class {}
+
+function App:init()
+end
+
+function App:update(dt)
+end
+
+function App:draw()
+end
---
+++ main.lua
@@ -0,0 +1,13 @@
+require "App"
+
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
