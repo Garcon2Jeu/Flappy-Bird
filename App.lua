9760808b98418409b0f86b6b3d1eac91f5598bce
Garcon2Jeu <104232891+Garcon2Jeu@users.noreply.github.com>
2023-10-20 15:59:15 +0200

    Configured App

--- App.lua
+++ App.lua
@@ -1,13 +1,52 @@
+WINDOW_WIDTH   = 1280
+WINDOW_HEIGHT  = 720
+VIRTUAL_WIDTH  = 413
+VIRTUAL_HEIGHT = 288
+CENTER_WIDTH   = WINDOW_WIDTH / 2
+CENTER_HEIGHT  = WINDOW_HEIGHT / 2
+
+
 Class = require "libraries.class"
-Push = require "libraries.push"
+Push  = require "libraries.push"
+
 
 App = Class {}
 
 function App:init()
+    math.randomseed(os.time())
+    self:setWindow()
+
+    self.keysPressed = {}
 end
 
 function App:update(dt)
+    self:quit()
+    self:flushKeys()
 end
 
 function App:draw()
+end
+
+function App:setWindow()
+    love.window.setTitle("Bird-Project")
+    love.graphics.setDefaultFilter("nearest", "nearest")
+    Push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = true,
+        vsync = true
+    })
+end
+
+function App:wasKeyPressed(key)
+    return self.keysPressed[key]
+end
+
+function App:flushKeys()
+    self.keysPressed = {}
+end
+
+function App:quit()
+    if self:wasKeyPressed("escape") then
+        love.event.quit()
+    end
 end
--- main.lua
+++ main.lua
@@ -1,13 +1,20 @@
 require "App"
 
 function love.load()
-
+    app = App()
 end
 
 function love.update(dt)
-
+    app:update(dt)
 end
 
 function love.draw()
+end
 
+function love.keypressed(key)
+    app.keysPressed[key] = true
+end
+
+function love.resize(w, h)
+    Push:resize(w, h)
 end
