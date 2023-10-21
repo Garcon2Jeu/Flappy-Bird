2429d0ed35e557c0a1e434d14bffd177c42a69c7
Garcon2Jeu <104232891+Garcon2Jeu@users.noreply.github.com>
2023-10-21 18:46:57 +0200

    Implemented OverState

--- states/OverState.lua
+++ states/OverState.lua
@@ -2,9 +2,18 @@ OverState = Class { __includes = BaseState }
 
 function OverState:init() end
 
-function OverState:update(dt) end
+function OverState:update(dt)
+    if app:wasKeyPressed("return") then
+        state:change("start")
+    end
+end
 
-function OverState:draw() end
+function OverState:draw()
+    love.graphics.setFont(app.fonts.big)
+    love.graphics.printf("Game Over", 0, CENTER_HEIGHT - 100, VIRTUAL_WIDTH, "center")
+    love.graphics.setFont(app.fonts.medium)
+    love.graphics.printf("Press Return to Play again", 0, CENTER_HEIGHT - 50, VIRTUAL_WIDTH, "center")
+end
 
 function OverState:enter(params) end
 
